b62191865e5a5d47851b78275dcc564bdaa7476e
otayuto4567 <otayuto4567@github.com>
2024-08-02 23:06:36 +0700

    init

---
+++ b-a-o-z-i-m-h-o-r-g.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         b-a-o-z-i-m-h-o-r-g
+// @namespace    https://github.com/otayuto4567/nay_nam_sau/raw/main/b-a-o-z-i-m-h-o-r-g.user.js
+// @version      2024.07.09
+// @description  down b-a-o-z-i-m-h-o-r-g
+// @author       You
+// @match        https://m.godamh.com/chapter/*
+// @match        https://baozimh.one/chapter/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=baozimh.org
+// @grant        GM_download
+// @grant        GM_info
+// @downloadURL  https://github.com/otayuto4567/nay_nam_sau/raw/main/b-a-o-z-i-m-h-o-r-g.user.js
+// @updateURL    https://github.com/otayuto4567/nay_nam_sau/raw/main/b-a-o-z-i-m-h-o-r-g.meta.js
+// ==/UserScript==
