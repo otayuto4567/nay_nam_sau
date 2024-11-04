21fd902edf1d6165a8f8175015d425ea2ca1e545
otayuto4567 <otayuto4567@github.com>
2024-11-04 13:11:15 +0700

    up

--- b-a-o-z-i-m-h-o-r-g.meta.js
+++ b-a-o-z-i-m-h-o-r-g.meta.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         b-a-o-z-i-m-h-o-r-g
 // @namespace    https://github.com/otayuto4567/nay_nam_sau/raw/main/b-a-o-z-i-m-h-o-r-g.user.js
-// @version      2024.07.09
+// @version      2024.11.04
 // @description  down b-a-o-z-i-m-h-o-r-g
 // @author       You
 // @match        https://m.godamh.com/chapter/*
 // @match        https://baozimh.one/chapter/*
+// @match        https://baozimh.org/manga/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=baozimh.org
-// @grant        GM_download
-// @grant        GM_info
+// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.48/dist/zip.min.js
 // @downloadURL  https://github.com/otayuto4567/nay_nam_sau/raw/main/b-a-o-z-i-m-h-o-r-g.user.js
 // @updateURL    https://github.com/otayuto4567/nay_nam_sau/raw/main/b-a-o-z-i-m-h-o-r-g.meta.js
 // ==/UserScript==
